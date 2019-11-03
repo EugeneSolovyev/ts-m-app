35c16f579e8fd2cae9370bc7b91f721cb023766e
evgeniycheban <mister.cheban@gmail.com>
2019-11-03 04:48:41 +0300

    stub phone verification

--- backend/user-service/src/main/java/com/musicapp/service/impl/PhoneVerificationServiceImpl.java
+++ backend/user-service/src/main/java/com/musicapp/service/impl/PhoneVerificationServiceImpl.java
@@ -5,16 +5,10 @@ import com.authy.api.Params;
 import com.authy.api.Verification;
 import com.google.i18n.phonenumbers.Phonenumber;
 import com.musicapp.exception.PhoneVerificationException;
-import com.musicapp.security.context.TokenContextHolder;
-import com.musicapp.service.PhoneVerificationService;
+import com.musicapp.service.AbstractPhoneVerificationService;
 import com.musicapp.service.TokenService;
 import com.musicapp.util.PhoneUtils;
-import com.musicapp.util.constants.ClaimConstants;
 import com.musicapp.util.constants.ProfileConstants;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -25,11 +19,14 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @Profile(ProfileConstants.PROD)
-@RequiredArgsConstructor
-public class PhoneVerificationServiceImpl implements PhoneVerificationService {
+public class PhoneVerificationServiceImpl extends AbstractPhoneVerificationService {
 
     private final AuthyApiClient authyApiClient;
-    private final TokenService tokenService;
+
+    public PhoneVerificationServiceImpl(TokenService tokenService, AuthyApiClient authyApiClient) {
+        super(tokenService);
+        this.authyApiClient = authyApiClient;
+    }
 
     @Override
     public void sendCode(String phone, String type) {
@@ -49,7 +46,7 @@ public class PhoneVerificationServiceImpl implements PhoneVerificationService {
     }
 
     @Override
-    public boolean verify(String phone, String code) {
+    protected boolean isValid(String phone, String code) {
         Phonenumber.PhoneNumber phoneNumber = PhoneUtils.parse(phone);
 
         Verification response = authyApiClient.getPhoneVerification().check(
@@ -58,29 +55,7 @@ public class PhoneVerificationServiceImpl implements PhoneVerificationService {
                 code
         );
 
-        boolean verified = response.isOk();
-
-        if (verified) {
-            Claims claims = Jwts.claims();
-            claims.put(ClaimConstants.VERIFIED_PHONE, phone);
-            String token = tokenService.generate(claims);
-            TokenContextHolder.setToken(token);
-        }
-
-        return verified;
-    }
-
-    @Override
-    public boolean isVerified(String phone) {
-        String token = TokenContextHolder.getToken();
-        if (StringUtils.isBlank(token)) {
-            return false;
-        }
-
-        Claims claims = tokenService.getClaims(token);
-        String verifiedPhone = claims.get(ClaimConstants.VERIFIED_PHONE, String.class);
-
-        return phone.equals(verifiedPhone);
+        return response.isOk();
     }
 
 }
--- backend/user-service/src/main/java/com/musicapp/service/stub/PhoneVerificationServiceStub.java
+++ backend/user-service/src/main/java/com/musicapp/service/stub/PhoneVerificationServiceStub.java
@@ -1,19 +1,22 @@
 package com.musicapp.service.stub;
 
-import com.musicapp.service.PhoneVerificationService;
+import com.musicapp.service.AbstractPhoneVerificationService;
+import com.musicapp.service.TokenService;
 import com.musicapp.util.constants.ProfileConstants;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Profile(ProfileConstants.LOCAL)
-@RequiredArgsConstructor
 @Slf4j
-public class PhoneVerificationServiceStub implements PhoneVerificationService {
+public class PhoneVerificationServiceStub extends AbstractPhoneVerificationService {
 
     private static final String STUB_PHONE_CODE = "9999";
+
+    public PhoneVerificationServiceStub(TokenService tokenService) {
+        super(tokenService);
+    }
 
     @Override
     public void sendCode(String phone, String type) {
@@ -21,15 +24,10 @@ public class PhoneVerificationServiceStub implements PhoneVerificationService {
     }
 
     @Override
-    public boolean verify(String phone, String code) {
-        log.debug("verify stub");
+    protected boolean isValid(String phone, String code) {
+        log.debug("isValid stub");
 
         return STUB_PHONE_CODE.equals(code);
-    }
-
-    @Override
-    public boolean isVerified(String phone) {
-        return true;
     }
 
 }
