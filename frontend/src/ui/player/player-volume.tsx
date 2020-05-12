446422b14d0329ba160c0586bc64b3cc542bffbb
Grosser0810 <web.bulanov@gmail.com>
2020-05-12 14:29:58 +0300

    Loop

--- frontend/src/ui/player/player-volume.tsx
+++ frontend/src/ui/player/player-volume.tsx
@@ -8,6 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 import {FavoriteButton} from "./player-favorite";
 import { Loop } from '@material-ui/icons';
+import LoopIcon from '@material-ui/icons/Loop';
 
 interface IAdditionalControlProps {
     volume: number;
@@ -68,7 +69,7 @@ const AdditionalControl = ({volume, onChange, loop = false, onToggleLoop}: IAddi
                 />
             </Tooltip>
             <Loop onClick={onToggleLoop} type='link' fontSize='small'>
-                <Icon type='retweet' />
+               <LoopIcon/>
             </Loop>
             <FavoriteButton />
         </AdditionalControlView>
