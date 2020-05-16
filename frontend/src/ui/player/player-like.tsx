ce47261658591dfd8cf58c402ac80995c93c11e8
Maksim Borovik <borovik.maksim@gmail.com>
2020-05-16 23:27:46 +0300

    added an action function

--- frontend/src/ui/player/player-like.tsx
+++ frontend/src/ui/player/player-like.tsx
@@ -1,17 +1,20 @@
-// import React, {useCallback, useEffect, useState} from "react";
 import React from "react";
 import IconButton from '@material-ui/core/IconButton'
 import {useSelector} from "react-redux";
 // import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import {toast} from "react-toastify";
 
 
-export const LikeButton: React.FC = () => {
+export const LikeButton: React.FC = (setToLike) => {
+  console.log(setToLike)
+
   /*const [favoriteTracksData, setFavoriteTrack] = useState(() => {
     const favoriteTracks = localStorage.getItem('favoriteTracks');
     return JSON.parse(favoriteTracks || '[]');
   });*/
-  const currentTrackId: string = useSelector((state: any): string => state.content.current.id);
+  const currentTrackId: string = useSelector((state: any): string => state.content.current && state.content.current.track_id);
+  const likes: string = useSelector((state: any): string => state.content.current && state.content.current.likes)
 
   /*const checkTrackId = useCallback((trackData: string[]) =>
     trackData.some((trackId: string) => trackId === currentTrackId), [currentTrackId]);*/
@@ -32,26 +35,31 @@ export const LikeButton: React.FC = () => {
   }, [favoriteTracksData]);*/
 
   const handleToLike = async (event: any) => {
-      console.log(currentTrackId)
+    console.log(currentTrackId)
 
-      await fetch(`${new URL(`http://5.101.51.243:8080/music-service/file`)}`, {
-        method: 'PUT',
-        body:
-          JSON.stringify({
-            track_id: currentTrackId,
-          }),
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      }).then(resolved => {console.log( 'ItemUpdated', resolved.json())}).then((data: any) => {console.log(data)}
-      ).catch(err => console.log( err ));
+    await fetch(`${new URL(`http://5.101.51.243:8080/music-service/file`)}`, {
+      method: 'PUT',
+      body:
+        JSON.stringify({
+          track_id: currentTrackId,
+        }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }).then(resolved => resolved.json()).then((data: any) => {console.log(data)}
+    ).catch(error => toast(error.message));
 
   }
 
   return (
-    <IconButton onClick={handleToLike} color="secondary" >
-      {/*{checkTrackId(favoriteTracksData) ? <FavoriteIcon /> : <FavoriteBorderIcon />}*/}
-      <FavoriteBorderIcon />
-    </IconButton>
+    <div>
+      {likes}
+      <IconButton onClick={handleToLike} color="secondary" >
+        {/*{checkTrackId(favoriteTracksData) ? <FavoriteIcon /> : <FavoriteBorderIcon />}*/}
+
+        <FavoriteBorderIcon />
+      </IconButton>
+    </div>
+
   )
 }
