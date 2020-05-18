b5d3a285382c7530ca7605faabf171d62dc2159f
Eugene Solovyev <eugenesolovyev92@gmail.com>
2020-05-18 19:55:40 +0300

    update code base with some improvements such as action creators were in the wrong place

--- frontend/src/actions/content.ts
+++ frontend/src/actions/content.ts
@@ -1,45 +1,86 @@
-import { find, propEq } from 'ramda'
-import {Content} from '../constants/content.enum'
-import { IContentReducerState } from '../reducers/reducers.d'
+import { find, propEq, head, path } from "ramda";
+import { Content } from "../constants/content.enum";
+import { IContentReducerState } from "../reducers/reducers.d";
 import HTTP from "../common/api";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
-type PossibleIDType = number | string
+type PossibleIDType = number | string;
 
-const TEST: string = "/music-service/file";
+const FILE_ENDPOINT: string = "/music-service/file";
+const DUPLICATE_ERROR_CODE: number = 11000;
 
-export const getTracks = (tracks: any) => ({
-	type: Content,
-	payload: (state: any) => ({
-		...state,
-		audio: tracks,
-	}),
-});
+export const getAllMusic = () => async (dispatch: any) => {
+  const response = await HTTP.get(FILE_ENDPOINT);
+
+  dispatch({
+    type: Content,
+    payload: <IContentReducerState>(state: any): IContentReducerState => ({
+      ...state,
+      audio: response,
+      current: head(response as any) || null,
+    }),
+  });
+};
+
+export const uploadMusic = (tracks: any) => async (dispatch: any) => {
+  const formData = new FormData();
+  Object.keys(tracks).forEach((key: string) => {
+    formData.append(key, tracks[key]);
+  });
+
+  try {
+    const { response, ...uploadedTrack }: any = await HTTP.post(
+      FILE_ENDPOINT,
+      formData
+    );
+    const isDuplicate: boolean =
+      path(["data", "error", "code"], response) === DUPLICATE_ERROR_CODE;
+
+    if (isDuplicate) throw Error("Duplicate track");
+
+    toast.success("Success");
+
+    dispatch({
+      type: Content,
+      payload: (state: any): any => ({
+        ...state,
+        audio: [...state.audio, uploadedTrack],
+      }),
+    });
+  } catch (error) {
+    toast.error(error.message);
+  }
+};
 
 export const setToPlay = (id: PossibleIDType) => ({
-	type: Content,
-	payload: <IContentReducerState>(state: any): IContentReducerState => ({
-		...state,
-		current: find(propEq('track_id', id))(state.audio)
-	}),
+  type: Content,
+  payload: <IContentReducerState>(state: any): IContentReducerState => ({
+    ...state,
+    current: find(propEq("track_id", id))(state.audio),
+  }),
 });
 
-export const setLikeToTrack = (trackId: string): any => async (dispatch: any, getState: Function) => {
-	try {
-		const current: any = await HTTP.put(TEST, {
-			track_id: trackId,
-		});
-		
-		dispatch({
-			type: Content,
-			payload: <IContentReducerState>(state: any): IContentReducerState => ({
-				...state,
-				audio: [...state.audio.filter(({track_id}: any) => track_id !== trackId), current],
-				current
-			})
-		})
+export const setLikeToTrack = (trackId: string): any => async (
+  dispatch: any,
+  getState: Function
+) => {
+  try {
+    const current: any = await HTTP.put(FILE_ENDPOINT, {
+      track_id: trackId,
+    });
 
-	} catch (error) {
-		toast(error.message);
-	}
-}
+    dispatch({
+      type: Content,
+      payload: <IContentReducerState>(state: any): IContentReducerState => ({
+        ...state,
+        audio: [
+          ...state.audio.filter(({ track_id }: any) => track_id !== trackId),
+          current,
+        ],
+        current,
+      }),
+    });
+  } catch (error) {
+    toast(error.message);
+  }
+};
--- frontend/src/ui/player/player.tsx
+++ frontend/src/ui/player/player.tsx
@@ -1,96 +1,110 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, {memo, useState, forwardRef, useCallback, useEffect} from 'react'
+import React, {
+  memo,
+  useState,
+  forwardRef,
+  useCallback,
+  useEffect,
+} from "react";
 
-import PlayerControl from './player-control'
-import PlayerTime from './player-time'
-import PlayerVolume from './player-volume'
-import PlayerView from './styles'
+import PlayerControl from "./player-control";
+import PlayerTime from "./player-time";
+import PlayerVolume from "./player-volume";
+import PlayerView from "./styles";
 
 interface IPlayerProps {
-    onClickNext(): void;
-    likes: number;
+  onClickNext(): void;
+  likes: number;
 }
 
-const EVENT_NAME: string = 'timeupdate'
+const EVENT_NAME: string = "timeupdate";
 
-const Player = forwardRef(({onClickNext, likes}: IPlayerProps, audioReference: any) => {
-    const [isPlaying, setPlaying] = useState<boolean>(true)
-    const [duration, setDuration] = useState<number>(0)
-    const [time, setTime] = useState<number>(0)
-    const [volume, setVolume] = useState<number>(50)
-    const [loop, setLoop] = useState<boolean>(false)
+const Player = forwardRef(
+  ({ onClickNext, likes }: IPlayerProps, audioReference: any) => {
+    const [isPlaying, setPlaying] = useState<boolean>(true);
+    const [duration, setDuration] = useState<number>(0);
+    const [time, setTime] = useState<number>(0);
+    const [volume, setVolume] = useState<number>(50);
+    const [loop, setLoop] = useState<boolean>(false);
 
-    const timeUpdate = ({target: {currentTime, duration}}: any) => {
-        setTime(currentTime);
-        setDuration(duration);
+    const timeUpdate = ({ target: { currentTime, duration } }: any) => {
+      setTime(currentTime);
+      setDuration(duration);
 
-        if (duration !== 0 && currentTime === duration) {
-            onClickNext();
-        }
-    }
+      if (duration !== 0 && currentTime === duration) {
+        onClickNext();
+      }
+    };
 
     const handleTogglePlay = useCallback(() => {
-        setPlaying(!isPlaying);
-        const {current} = audioReference
-        isPlaying ? current.pause() : current.play()
-    }, [audioReference, isPlaying])
+      setPlaying(!isPlaying);
+      const { current } = audioReference;
+      isPlaying ? current.pause() : current.play();
+    }, [audioReference, isPlaying]);
 
     const handleClickNext = useCallback(() => {
-        onClickNext()
-    }, [onClickNext])
+      onClickNext();
+    }, [onClickNext]);
 
-    const handleChangeTime = useCallback((event: React.ChangeEvent<{}>, value: any) => {
-        const {current} = audioReference;
+    const handleChangeTime = useCallback(
+      (event: React.ChangeEvent<{}>, value: any) => {
+        const { current } = audioReference;
         current.currentTime = value;
-    }, [audioReference])
+      },
+      [audioReference]
+    );
 
-    const handleChangeVolume = useCallback((value: any) => {
-        const {current} = audioReference;
+    const handleChangeVolume = useCallback(
+      (value: any) => {
+        const { current } = audioReference;
         setVolume(value);
         current.volume = value / 100;
-    }, [audioReference])
+      },
+      [audioReference]
+    );
 
     const handleToggleLoop = useCallback(() => {
-        const {current} = audioReference;
-        setLoop(!loop);
-        current.loop = !loop;
-    }, [audioReference, loop])
+      const { current } = audioReference;
+      setLoop(!loop);
+      current.loop = !loop;
+    }, [audioReference, loop]);
 
     useEffect(() => {
-        const {current} = audioReference;
-        current.play();
+      const { current } = audioReference;
+      current.play();
 
-        current.addEventListener(EVENT_NAME, timeUpdate)
+      current.addEventListener(EVENT_NAME, timeUpdate);
 
-        return () => {
-            current.removeEventListener(EVENT_NAME, timeUpdate)
-        }
-    }, [audioReference])
+      return () => {
+        current.removeEventListener(EVENT_NAME, timeUpdate);
+      };
+    }, [audioReference]);
 
     return (
-        <>
-            <PlayerView>
-                <PlayerControl
-                    isPlaying={isPlaying}
-                    onTogglePlay={handleTogglePlay}
-                    onClickNext={handleClickNext}
-                />
-                <PlayerTime
-                    currentTime={time}
-                    duration={duration}
-                    onHandleChangeTime={handleChangeTime}
-                />
-                <PlayerVolume
-                    volume={volume}
-                    onChange={handleChangeVolume}
-                    loop={loop}
-                    onToggleLoop={handleToggleLoop}
-                    likes={likes}
-                />
-            </PlayerView>
-            <audio ref={audioReference}/>
-        </>
-    )
-})
+      <>
+        <PlayerView>
+          <PlayerControl
+            isPlaying={isPlaying}
+            onTogglePlay={handleTogglePlay}
+            onClickNext={handleClickNext}
+          />
+          <PlayerTime
+            currentTime={time}
+            duration={duration}
+            onHandleChangeTime={handleChangeTime}
+          />
+          <PlayerVolume
+            volume={volume}
+            onChange={handleChangeVolume}
+            loop={loop}
+            onToggleLoop={handleToggleLoop}
+            likes={likes}
+          />
+        </PlayerView>
+        <audio ref={audioReference} preload="metadata" />
+      </>
+    );
+  }
+);
 
-export default memo(Player)
+export default memo(Player);
