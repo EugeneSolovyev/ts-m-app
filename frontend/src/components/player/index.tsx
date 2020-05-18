b50498a90c694efc42fbaf3a8cf537f23c532bd2
Grosser0810 <web.bulanov@gmail.com>
2020-05-18 11:12:03 +0300

    get one track in player

--- frontend/src/components/player/index.tsx
+++ frontend/src/components/player/index.tsx
@@ -1,100 +1,103 @@
-import React, { createRef } from 'react';
-import { pathOr, path } from 'ramda';
-import { bindActionCreators } from 'redux';
-import { connect } from '../../helpers/connect';
-import { IAudio } from '../../reducers/reducers.d';
-import { IContentReducerState } from '../../reducers/reducers'
-import { IStore } from '../../store'
-import { setToPlay } from '../../actions/content'
-import { getAllMusic } from './fetchMusic'
+import React, {createRef} from 'react';
+import {pathOr, path} from 'ramda';
+import {bindActionCreators} from 'redux';
+import {connect} from '../../helpers/connect';
+import {IAudio} from '../../reducers/reducers.d';
+import {IContentReducerState} from '../../reducers/reducers'
+import {IStore} from '../../store'
+import {setToPlay} from '../../actions/content'
+import {getAllMusic} from './fetchMusic'
 import PlayerComponent from '../../ui/player'
-import { currentPlaySelector } from './selector'
-import { baseUrl } from "../../constants/content.enum"
+import {currentPlaySelector} from './selector'
+import {baseUrl} from "../../constants/content.enum"
 
 interface ITrackList {
-	value: IAudio;
-	next: IAudio | null;
+    value: IAudio;
+    next: IAudio | null;
 }
 
 interface IPlayerProp {
-	content: ITrackList;
-	setToPlay(track_id: string | number): void;
+    content: ITrackList;
+
+    setToPlay(track_id: string | number): void;
 }
 
 interface IPlayerState {
-	list: ITrackList;
-	current: IContentReducerState
+    list: ITrackList;
 }
 
 
-const DEFAULT_VALUE: any = { track_id: null };
-const currentTrackSelector = (state: IStore): IAudio => pathOr({}, [ 'content', 'current' ], state) as IAudio;
+const DEFAULT_VALUE: any = {track_id: null};
+const currentTrackSelector = (state: IStore): IAudio => pathOr({}, ['content', 'current'], state) as IAudio;
 @(connect(
-	(state: IStore) => ({
-		content: currentPlaySelector(state),
-		current: currentTrackSelector(state)
-	}),
-	(dispatch) =>
-		bindActionCreators(
-			{
-				setToPlay,
-				getAllMusic,
-			},
-			dispatch
-		)
+    (state: IStore) => ({
+        content: currentPlaySelector(state),
+        current: currentTrackSelector(state)
+    }),
+    (dispatch) =>
+        bindActionCreators(
+            {
+                setToPlay,
+                getAllMusic,
+            },
+            dispatch
+        )
 ) as any)
 export default class Player extends React.Component<any, IPlayerState> {
-	constructor(props: any) {
-		super(props);
-		this.state = {
-			list: this.props.content,
-			current: this.props.current,
-		};
-	}
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            list: this.props.content,
+        };
+    }
 
-	content = createRef<HTMLAudioElement>();
+    content = createRef<HTMLAudioElement>();
 
-	componentDidMount() {
-		this.props.getAllMusic();
-	}
-	componentDidUpdate(prevProps: any, prevState: any) {
+    componentDidMount() {
+        this.props.getAllMusic();
+    }
 
-		if (this.props.content !== prevProps.content) {
-			this.setState((state, props) => ({
-				list: this.props.content,
-			}));
-		}
-		if (this.state.list !== prevState.list) {
-			const { list } = this.state;
-			if (list) {
-				this.content.current.src = `${baseUrl}/${list.value.track_id}`;
-			}
-		}
-	}
+    componentDidUpdate(prevProps: any, prevState: any) {
+        if (this.props.content !== prevProps.content) {
+            this.setState((state, props) => ({
+                list: this.props.content,
+            }));
+        }
+        if (this.props.current !== prevProps.current) {
+            const {current} = this.props;
+            return this.content.current.src = `${baseUrl}/${current.track_id}`;
+        }
+        if (this.state.list !== prevState.list) {
+            const {list} = this.state;
+            if (list) {
+                this.content.current.src = `${baseUrl}/${list.value.track_id}`;
+            }
+        }
+    }
 
-	postHandleNext = (): void => {
-		const { setToPlay } = this.props;
-		const { list } = this.state;
-		const { track_id } = pathOr(DEFAULT_VALUE, ['value'], list);
-		if (track_id) {
-			this.content.current.src = `${baseUrl}/${track_id}`;
-			setToPlay(track_id);
-		} else {
-			this.content.current.pause();
-		}
-	};
+    postHandleNext = (): void => {
+        const {setToPlay} = this.props;
+        const {list} = this.state;
+        const {track_id} = pathOr(DEFAULT_VALUE, ['value'], list);
+        if (track_id) {
+            this.content.current.src = `${baseUrl}/${track_id}`;
+            setToPlay(track_id);
+        } else {
+            this.content.current.pause();
+        }
+    };
 
-	handleClickNext = (): void => {
-		this.setState(
-			(prev: any) => ({
-				list: path(['list', 'next'], prev),
-			}),
-			this.postHandleNext
-		);
-	};
+    handleClickNext = (): void => {
+        this.setState(
+            (prev: any) => ({
+                list: path(['list', 'next'], prev),
+            }),
+            this.postHandleNext
+        );
+    };
 
-	render() {
+    render() {
 		console.log(this.props)
-		return <PlayerComponent onClickNext={this.handleClickNext} ref={this.content} />;
-	}
+        return <PlayerComponent onClickNext={this.handleClickNext} ref={this.content}/>;
+    }
 }
