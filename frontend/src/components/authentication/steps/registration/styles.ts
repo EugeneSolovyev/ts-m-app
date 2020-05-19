63944a483e613df634883357b678a7eae95e801e
Serhii Zaklevskyi <serhii.jh@gmail.com>
2020-05-19 13:15:23 +0300

    Fix display issue in registration form
    Form was overreached screen and scroll didnt appaer. Commit makes error fields and margin smaller and adds scroll when form has overflow
    
    Issue: AMU-56

--- frontend/src/components/authentication/steps/registration/styles.ts
+++ frontend/src/components/authentication/steps/registration/styles.ts
@@ -1,17 +1,21 @@
-import styled from 'styled-components';
-import { Form } from 'formik';
+import styled from 'styled-components'
+import { Form } from 'formik'
 
 export const RegistrWrapper = styled(Form)`
+	max-height: 80vh;
+	overflow: auto;
+
 	.error {
 		color: red;
 		margin-left: 0.5rem;
+		margin-bottom: 0.5rem;
+		font-size: 0.8rem;
 	}
 	.MuiButtonBase-outlined {
-    margin-top: 1rem;
+		margin-top: 1rem;
 	}
 	.MuiFormControl-root {
-		margin-top: 1rem;
 		margin-bottom: 0.5rem;
 		width: 100%;
 	}
-`;
+`
