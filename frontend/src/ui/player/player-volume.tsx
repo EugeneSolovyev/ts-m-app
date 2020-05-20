610c7df398eda9b40d5cea19c20825f081426bea
Grosser0810 <web.bulanov@gmail.com>
2020-05-20 11:24:49 +0300

    recovery volume slider

--- frontend/src/ui/player/player-volume.tsx
+++ frontend/src/ui/player/player-volume.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Popover, Icon } from 'antd';
 import { Loop } from '@material-ui/icons';
 import { VolumeSlider, AdditionalControlView } from './styles';
 import {FavoriteButton} from "./player-favorite";
 import {LikeButton} from "./player-like";
+import {makeStyles, Theme, createStyles} from '@material-ui/core/styles';
+import {Slider} from '@material-ui/core';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
+import VolumeOffIcon from '@material-ui/icons/VolumeOff';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+
 
 interface IAdditionalControlProps {
   volume: number;
@@ -13,6 +20,23 @@ interface IAdditionalControlProps {
   likes: number;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        arrow: {
+            color: '#2c3e50'
+        },
+        tooltip: {
+            backgroundColor: '#2c3e50',
+            width: 200,
+            height: 35,
+            paddingLeft: 20,
+            paddingRight: 20,
+            boxShadow: ' 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'
+        },
+    }),
+);
+
+
 const AdditionalControl = ({
   volume,
   onChange,
@@ -20,13 +44,41 @@ const AdditionalControl = ({
   onToggleLoop,
   likes
 }: IAdditionalControlProps) => {
-  return (
+    const [sound, setSound] = useState(true);
+    const [soundMemory, setSoundMemory] = useState(volume);
+    const classes = useStyles();
+
+    const handleChangeVolume = (event: any, newValue: any) => {
+        if (!sound) setSound(true);
+        setSoundMemory(newValue);
+        onChange(soundMemory)
+    };
+
+    useEffect(() => {
+        if (!sound) {
+            onChange(0)
+        } else {
+            onChange(soundMemory)
+        }
+    });
+
+    return (
     <AdditionalControlView loop={loop}>
-      <Popover
-        content={<VolumeSlider vertical value={volume} onChange={onChange} />}>
-        <Icon type='sound' />
-      </Popover>
-      <Loop onClick={onToggleLoop} type='link' fontSize='small'>
+        <Tooltip title={
+            <Slider
+                value={volume}
+                color='secondary'
+                onChange={handleChangeVolume}
+            />
+        } arrow interactive classes={classes}>
+            <IconButton
+                children={volume > 0 ? <VolumeUpIcon/> : <VolumeOffIcon/>}
+                onClick={() => setSound(!sound)}
+                color='inherit'
+            />
+        </Tooltip>
+
+        <Loop onClick={onToggleLoop} type='link' fontSize='small'>
         <Icon type='retweet' />
       </Loop>
       <FavoriteButton />
